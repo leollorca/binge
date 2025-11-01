37ff4776fbcc04f67276487554d64632f91a633a
Léo Llorca <leollorca@proton.me>
2025-11-01 10:41:18 +0100

    delete some useless files and edit README.md

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ const inter = Inter({
 
 export const metadata = {
   title: "Binge",
-  description: "Cleanest player.",
+  description: "Clean IPTV player",
 };
 
 export default function RootLayout({ children }) {
